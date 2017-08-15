938fe4a41c0ae111f8d9617bd9efefcd6f6f918f
Marcos Pereira <marcos.pereira@agriness.com>
2017-08-15 11:03:30 -0300

    Updated docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # python-keycloak documentation build configuration file, created by
-# sphinx-quickstart on Tue Aug 15 10:44:03 2017.
+# sphinx-quickstart on Tue Aug 15 11:02:59 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -31,7 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +40,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = '.md'
 
 # The master toctree document.
 master_doc = 'index'
@@ -69,7 +69,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
--- docs/source/index.md
+++ docs/source/index.md
@@ -1,5 +1,5 @@
 .. python-keycloak documentation master file, created by
-   sphinx-quickstart on Tue Aug 15 10:44:03 2017.
+   sphinx-quickstart on Tue Aug 15 11:02:59 2017.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
