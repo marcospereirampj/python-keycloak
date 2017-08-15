0f9b6190819633198530f032b5cb79a83d468d76
Marcos Pereira <marcos.pereira@agriness.com>
2017-08-15 11:34:37 -0300

    Updated setup.

--- setup.py
+++ setup.py
@@ -9,8 +9,18 @@ setup(
     license='GNU General Public License - V3',
     author='Marcos Pereira',
     author_email='marcospereira.mpj@gmail.com',
+    copyright = 'Copyright (c) 2017 Marcos Pereira',
     keywords='keycloak openid',
     description=u'python-keycloak is a Python package providing access to the Keycloak API.',
     packages=['keycloak'],
     install_requires=['requests==2.18.3', 'httmock==1.2.5'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Utilities'
+    ]
 )
