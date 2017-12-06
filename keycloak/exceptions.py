d5b48c2d68778b6b5c56148a5e890435f4878053
Shon T. Urbas <shon.urbas@gmail.com>
2017-12-06 11:05:50 -0500

    no need for simplejson

--- keycloak/exceptions.py
+++ keycloak/exceptions.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import requests
-from simplejson import JSONDecodeError
+from json import JSONDecodeError
 
 
 class KeycloakError(Exception):
