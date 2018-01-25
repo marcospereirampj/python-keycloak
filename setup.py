56489b392c27e2e0d79eb4a5b30b5698dc221a13
Marcos Pereira <marcospereira.mpj@gmail.com>
2018-01-25 20:30:30 -0200

    Updated setup.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='python-keycloak',
-    version='0.11.1',
+    version='0.12.0',
     url='https://bitbucket.org/agriness/python-keycloak',
     license='GNU General Public License - V3',
     author='Marcos Pereira',
