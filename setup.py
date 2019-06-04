3352c10e39d866858b824b6b62b6943709786b7d
Marcos Pereira <marcospereira.mpj@gmailcom>
2019-06-04 09:13:37 -0300

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-keycloak',
-    version='0.17.1',
+    version='0.17.2',
     url='https://github.com/marcospereirampj/python-keycloak',
     license='The MIT License',
     author='Marcos Pereira',
