0ebcf990941f600e6ff562091a81cbeafbd21f3e
Marcos Pereira <marcos.pereira@mercadolivre.com>
2021-06-01 14:47:54 -0300

    Release 0.25.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-keycloak',
-    version='0.24.0',
+    version='0.25.0',
     url='https://github.com/marcospereirampj/python-keycloak',
     license='The MIT License',
     author='Marcos Pereira',
