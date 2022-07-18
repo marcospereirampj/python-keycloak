83d6cf36f637aeb613656b1897dc73f38a76d044
Luca Paganin <luca.paganin@rulex.ai>
2022-07-18 21:41:35 +0200

    fix: linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
 """Fixtures for tests."""
 
+import ipaddress
 import os
 import uuid
-import ipaddress
 from datetime import datetime, timedelta
 
+import pytest
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.x509.oid import NameOID
-import pytest
 
 from keycloak import KeycloakAdmin, KeycloakOpenID
 
