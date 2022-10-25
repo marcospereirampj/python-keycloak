eda6a2762ca36d933c5656f25c8d013d8e62d93b
Richard Nemeth <ryshoooo@gmail.com>
2022-10-25 06:38:59 +0000

    ci: fix docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -180,7 +180,15 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "python-keycloak", "python-keycloak Documentation", [author], 1)]
+man_pages = [
+    (
+        master_doc,
+        "python-keycloak",
+        "python-keycloak Documentation",
+        [author],
+        1,
+    )
+]
 
 
 # -- Options for Texinfo output -------------------------------------------
