3d6ee7136849825be64facc3452b71e77e404a95
hadeer-elsaeed <hadeerelsaeed00@gmail.com>
2023-04-06 01:20:26 +0200

    fix: test case of create client authz scope permissions

--- tests/test_keycloak_admin.py
+++ tests/test_keycloak_admin.py
@@ -1669,7 +1669,7 @@ def test_enable_token_exchange(admin: KeycloakAdmin, realm: str):
     # Create permissions on the target client to reference this policy
     admin.create_client_authz_scope_permission(
         payload={
-            "id": token_exchange_permission_id,
+            "id": token_exchange_scope_id,
             "name": "test-permission",
             "type": "scope",
             "logic": "POSITIVE",
@@ -1681,7 +1681,7 @@ def test_enable_token_exchange(admin: KeycloakAdmin, realm: str):
         client_id=realm_management_id,
     )
     permission_name = admin.get_client_authz_scope_permission(
-        client_id=realm_management_id, scope_id=token_exchange_permission_id
+        client_id=realm_management_id, scope_id=token_exchange_scope_id
     )["name"]
     assert permission_name == "test-permission"
     with pytest.raises(KeycloakPostError) as err:
